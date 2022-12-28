fbb19eaf94e1eb1fd431fd77dca0fc018c35b2c8
alajido <96017433+Alajido@users.noreply.github.com>
2022-12-28 10:16:54 +0100

    commit

---
+++ scripts/promise.js
@@ -0,0 +1,16 @@
+const posts = [
+    {title: "post one", body: "this is post one"},
+    {title: "post two", body: "this is post two"},
+];
+
+const getPost = () => {
+    setTimeout(() => {
+        let outPut = ''
+        posts.forEach((post) => {
+            outPut += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = outPut;
+    },2000)
+}
+
+getPost()
